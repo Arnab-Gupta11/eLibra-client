1c6f678eec49498df59653506b270186f93af6a6
Arnab Gupta <arnabgupta1111@gmail.com>
2023-11-08 20:55:03 +0600

    Read story button functionality done

--- src/Pages/Home/Category/ReadStory.jsx
+++ src/Pages/Home/Category/ReadStory.jsx
@@ -3,9 +3,16 @@ import { useLoaderData } from "react-router-dom";
 const ReadStory = () => {
   const story = useLoaderData();
   console.log(story);
+  const { short_description, name, authore_name } = story;
   return (
-    <div>
-      <h2>read</h2>
+    <div className="">
+      <div className="mt-15">
+        <div className="max-w-7xl mx-auto bg-gray-50 mt-14 text-center">
+          <h1 className="text-xl font-bold text-[#1A2D62]">{name}</h1>
+          <p className="tex-lg">{authore_name}</p>
+          <p className="text-justify text-gray-400">{short_description}</p>
+        </div>
+      </div>
     </div>
   );
 };
